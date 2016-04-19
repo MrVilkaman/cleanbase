f99966946ffd1daac8c883d7d51e546154df3d65
Zahar Zolotarev <zahar.zolotarev@gmail.com>
2016-04-19 14:50:56 +0300

    Added robolectric tests

---
+++ app/src/integrationTests/java/com/github/mrvilkaman/namegenerator/TestApplication.java
@@ -0,0 +1,13 @@
+package com.github.mrvilkaman.namegenerator;
+
+import android.annotation.SuppressLint;
+
+import com.github.mrvilkaman.namegenerator.presentationlayer.app.App;
+
+/**
+ * Created by Zahar on 14.04.16.
+ */
+@SuppressLint("Registered")
+public class TestApplication extends App {
+
+}
