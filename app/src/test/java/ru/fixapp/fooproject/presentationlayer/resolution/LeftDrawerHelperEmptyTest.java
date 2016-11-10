f4868831de8ab83aba1c550247588417ecd812c2
Zahar Zolotarev <zahar.zolotarev@gmail.com>
2016-11-10 22:17:57 +0300

    added tests for empty drawer helper

---
+++ app/src/test/java/ru/fixapp/fooproject/presentationlayer/resolution/LeftDrawerHelperEmptyTest.java
@@ -0,0 +1,34 @@
+package ru.fixapp.fooproject.presentationlayer.resolution;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import ru.fixapp.fooproject.presentationlayer.fragments.core.BaseFragment;
+import ru.fixapp.fooproject.testsutils.BaseTestCase;
+
+
+public class LeftDrawerHelperEmptyTest extends BaseTestCase {
+
+
+
+	// also for good test coverage
+	@Test
+	public void testAll(){
+	    // Arrange
+		LeftDrawerHelper drawerHelper = new LeftDrawerHelperEmpty();
+
+		// Act
+		drawerHelper.open();
+		drawerHelper.close();
+		drawerHelper.isOpen();
+		drawerHelper.close(() -> {});
+		drawerHelper.getDrawerLayout();
+		drawerHelper.getDrawerContentFrame();
+		BaseFragment drawerFragment = drawerHelper.getDrawerFragment();
+		boolean hasDrawer = drawerHelper.hasDrawer();
+
+		// Assert
+		Assert.assertFalse(hasDrawer);
+		Assert.assertNull(drawerFragment);
+	}
+}
