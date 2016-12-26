dbc987fb8aa0b2f851b1f99f00f911704d42fb0d
Zahar Zolotarev <zahar.zolotarev@gmail.com>
2016-12-26 12:39:38 +0300

    move tests utils to another module

--- app/src/test/java/ru/fixapp/fooproject/presentationlayer/fragments/BaseFragmentTest.java
+++ app/src/test/java/ru/fixapp/fooproject/presentationlayer/fragments/BaseFragmentTest.java
@@ -1,16 +1,13 @@
 package ru.fixapp.fooproject.presentationlayer.fragments;
 
 
-import com.github.mrvilkaman.core.BuildConfig;
-
 import org.junit.Before;
 import org.junit.Rule;
-import org.junit.runner.RunWith;
+import org.mockito.Matchers;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
-import org.robolectric.RobolectricTestRunner;
-import org.robolectric.annotation.Config;
 
 import ru.fixapp.fooproject.presentationlayer.activities.BaseActivityView;
 import ru.fixapp.fooproject.presentationlayer.fragments.core.BaseFragment;
@@ -18,13 +15,6 @@ import ru.fixapp.fooproject.presentationlayer.resolution.ThrowableResolver;
 import ru.fixapp.fooproject.presentationlayer.resolution.UIResolver;
 import ru.fixapp.fooproject.presentationlayer.resolution.toolbar.IToolbar;
 
-import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.doReturn;
-
-@RunWith(RobolectricTestRunner.class)
-@Config(constants = BuildConfig.class, sdk = 18, /*application = TestApplication.class,*/
-		manifest = "src/main/AndroidManifest.xml")
 public abstract class BaseFragmentTest {
 
 	@Mock protected IToolbar toolbar;
@@ -42,10 +32,10 @@ public abstract class BaseFragmentTest {
 	}
 
 	protected void initDep(BaseFragment fragment) {
-		doNothing().when(fragment)
-				.daggerInject(any());
-		doReturn(null).when(fragment)
-				.getComponent(any());
+		Mockito.doNothing().when(fragment)
+				.daggerInject(Matchers.any());
+		Mockito.doReturn(null).when(fragment)
+				.getComponent(Matchers.any());
 		fragment.uiResolver = uiRes;
 		fragment.toolbar = toolbar;
 		fragment.activityView = activityView;
