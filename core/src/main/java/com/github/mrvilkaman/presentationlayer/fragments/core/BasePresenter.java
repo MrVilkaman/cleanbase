acbb6bd25b7a3be961e7a36dc5a1609d1dd60c77
Zahar Zolotarev <zahar.zolotarev@gmail.com>
2017-01-14 23:42:56 +0300

    add UIResolver to view subscriber and base presenter

--- core/src/main/java/com/github/mrvilkaman/presentationlayer/fragments/core/BasePresenter.java
+++ core/src/main/java/com/github/mrvilkaman/presentationlayer/fragments/core/BasePresenter.java
@@ -1,6 +1,7 @@
 package com.github.mrvilkaman.presentationlayer.fragments.core;
 
 import com.github.mrvilkaman.domainlayer.providers.SchedulersProvider;
+import com.github.mrvilkaman.presentationlayer.resolution.UIResolver;
 import com.github.mrvilkaman.presentationlayer.subscriber.ViewSubscriber;
 
 import javax.inject.Inject;
@@ -16,6 +17,7 @@ public class BasePresenter<V extends BaseView> {
 	private CompositeSubscription compositeSubscription = new CompositeSubscription();
 
 	private SchedulersProvider schedulersProvider;
+	private UIResolver uiResolver;
 
 	public BasePresenter() {
 	}
@@ -23,6 +25,10 @@ public class BasePresenter<V extends BaseView> {
 	@Inject
 	public final void setSchedulersProvider(SchedulersProvider schedulersProvider) {
 		this.schedulersProvider = schedulersProvider;
+	}
+	@Inject
+	public final void setSchedulersProvider(UIResolver uiResolver) {
+		this.uiResolver = uiResolver;
 	}
 
 	public void onViewAttached() {
@@ -36,6 +42,10 @@ public class BasePresenter<V extends BaseView> {
 		return view;
 	}
 
+	public final UIResolver uiResolver() {
+		return uiResolver;
+	}
+
 	public final void setView(V view) {
 		this.view = view;
 	}
@@ -44,9 +54,12 @@ public class BasePresenter<V extends BaseView> {
 		subscribe(observable, new ViewSubscriber<>());
 	}
 
+	@SuppressWarnings("unchecked")
 	protected final <T> void subscribe(Observable<T> observable, Subscriber<T> subscriber) {
 		if (subscriber instanceof ViewSubscriber) {
-			((ViewSubscriber) subscriber).setView(view);
+			ViewSubscriber viewSubscriber = (ViewSubscriber) subscriber;
+			viewSubscriber.setView(view);
+			viewSubscriber.setUiResolver(uiResolver);
 		}
 		compositeSubscription.add(observable.subscribe(subscriber));
 	}
