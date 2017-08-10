1adbb43a54c588ef5cb2ac7ece260f252a42783a
Zahar <zahar.zolotarev@gmail.com>
2017-08-10 16:52:40 +0300

    fixed RxLoadWrapper

--- core/src/main/java/com/github/mrvilkaman/datalayer/tools/RxLoadWrapperHolder.java
+++ core/src/main/java/com/github/mrvilkaman/datalayer/tools/RxLoadWrapperHolder.java
@@ -6,7 +6,12 @@ import android.support.annotation.NonNull;
 import com.github.mrvilkaman.domainlayer.models.DataErrorWrapper;
 
 import io.reactivex.Observable;
+import io.reactivex.ObservableSource;
 import io.reactivex.ObservableTransformer;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Action;
+import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Function;
 import io.reactivex.subjects.BehaviorSubject;
 import io.reactivex.subjects.PublishSubject;
 
@@ -29,38 +34,99 @@ public class RxLoadWrapperHolder {
 	}
 
 	public <T> ObservableTransformer<T, T> bindToLoad() {
-		return this::bind;
+		return new ObservableTransformer<T, T>() {
+			@Override
+			public ObservableSource<T> apply(Observable<T> observable) {
+				return RxLoadWrapperHolder.this.bind(observable);
+			}
+		};
 	}
 
 	public <T> ObservableTransformer<T, T> bindToLoadSilent() {
-		return observable -> bind(observable).onErrorResumeNext(Observable.empty());
+		return new ObservableTransformer<T, T>() {
+			@Override
+			public ObservableSource<T> apply(Observable<T> observable) {
+				return RxLoadWrapperHolder.this.bind(observable)
+						.onErrorResumeNext(Observable.empty());
+			}
+		};
 	}
 
 	public <D> Observable<DataErrorWrapper<D>> modifyStream(Observable<D> observable) {
-		return observable.map(d -> new DataErrorWrapper<>(d, progress.getValue()))
+		return observable.map(new Function<D, DataErrorWrapper<D>>() {
+			@Override
+			public DataErrorWrapper<D> apply(D d) throws Exception {
+				return new DataErrorWrapper<>(d, progress.getValue());
+			}
+		})
 				.mergeWith(error.map(
-						throwable -> new DataErrorWrapper<>(throwable, progress.getValue())))
-				.mergeWith(progress.map(DataErrorWrapper::new));
+						new Function<Throwable, DataErrorWrapper<D>>() {
+							@Override
+							public DataErrorWrapper<D> apply(Throwable throwable) throws Exception {
+								return new DataErrorWrapper<>(throwable, progress.getValue());
+							}
+						}))
+				.mergeWith(progress.map(new Function<Boolean, DataErrorWrapper<D>>() {
+					@Override
+					public DataErrorWrapper<D> apply(Boolean progress1) throws Exception {
+						return new DataErrorWrapper<>(progress1);
+					}
+				}));
 	}
 
 	public <T> ObservableTransformer<T, DataErrorWrapper<T>> modifyStream() {
-		return this::modifyStream;
+		return new ObservableTransformer<T, DataErrorWrapper<T>>() {
+			@Override
+			public ObservableSource<DataErrorWrapper<T>> apply(Observable<T> observable) {
+				return RxLoadWrapperHolder.this.modifyStream(observable);
+			}
+		};
 	}
 
 	public <D> Observable<DataErrorWrapper<D>> modifySingle(Observable<D> observable) {
-		return observable.map(d -> new DataErrorWrapper<>(d, false))
+		return observable.map(new Function<D, DataErrorWrapper<D>>() {
+			@Override
+			public DataErrorWrapper<D> apply(D d) throws Exception {
+				return new DataErrorWrapper<>(d, false);
+			}
+		})
 				.startWith(new DataErrorWrapper<>(true))
-				.onErrorReturn(throwable -> new DataErrorWrapper(throwable, false));
+				.onErrorReturn(new Function<Throwable, DataErrorWrapper<D>>() {
+					@Override
+					public DataErrorWrapper<D> apply(Throwable throwable) throws Exception {
+						return new DataErrorWrapper(throwable, false);
+					}
+				});
 	}
 
 	public <T> ObservableTransformer<T, DataErrorWrapper<T>> modifySingle() {
-		return this::modifySingle;
+		return new ObservableTransformer<T, DataErrorWrapper<T>>() {
+			@Override
+			public ObservableSource<DataErrorWrapper<T>> apply(Observable<T> observable) {
+				return RxLoadWrapperHolder.this.modifySingle(observable);
+			}
+		};
 	}
 
 	@NonNull
 	private <T> Observable<T> bind(Observable<T> observable) {
-		return observable.doOnSubscribe(disposable -> progress.onNext(true))
-				.doOnTerminate(() -> progress.onNext(false))
-				.doOnError(error::onNext);
+		return observable.doOnSubscribe(new Consumer<Disposable>() {
+			@Override
+			public void accept(Disposable disposable) throws Exception {
+				progress.onNext(true);
+			}
+		})
+				.doOnTerminate(new Action() {
+					@Override
+					public void run() throws Exception {
+						progress.onNext(false);
+					}
+				})
+				.doOnError(new Consumer<Throwable>() {
+					@Override
+					public void accept(Throwable t) throws Exception {
+						error.onNext(t);
+					}
+				});
 	}
 }
