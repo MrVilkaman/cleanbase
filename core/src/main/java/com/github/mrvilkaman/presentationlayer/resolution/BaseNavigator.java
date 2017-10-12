26a34083f995fa111cf7f481caf8746f89837e66
Zahar <zahar.zolotarev@gmail.com>
2017-10-12 09:06:40 +0300

    Move base navigator to core lib

---
+++ core/src/main/java/com/github/mrvilkaman/presentationlayer/resolution/BaseNavigator.java
@@ -0,0 +1,178 @@
+package com.github.mrvilkaman.presentationlayer.resolution;
+
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.github.mrvilkaman.core.R;
+import com.github.mrvilkaman.di.INeedActivityViewNotify;
+import com.github.mrvilkaman.presentationlayer.fragments.core.OnBackPressedListener;
+import com.github.mrvilkaman.presentationlayer.resolution.drawer.LeftDrawerHelper;
+import com.github.mrvilkaman.presentationlayer.resolution.toolbar.ToolbarResolver;
+
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Completable;
+import io.reactivex.functions.Action;
+import ru.terrakok.cicerone.android.SupportAppNavigator;
+import ru.terrakok.cicerone.commands.Back;
+import ru.terrakok.cicerone.commands.Command;
+import ru.terrakok.cicerone.commands.Forward;
+
+public abstract class BaseNavigator extends SupportAppNavigator implements INeedActivityViewNotify {
+
+
+	private final AppCompatActivity activity;
+	private final int containerId;
+	@Nullable private final ToolbarResolver toolbarResolver;
+	@Nullable private final LeftDrawerHelper leftDrawerHelper;
+	private final FragmentManager fragmentManager;
+
+	private boolean doubleBackToExitPressedOnce = false;
+	private ToolbarResolver.ToolbarResolverCallback value = new ToolbarResolver.ToolbarResolverCallback() {
+		@Override
+		public void onClickHome() {
+			if (fragmentManager.getBackStackEntryCount() <= 1 && activity.isTaskRoot()) {
+				if (leftDrawerHelper != null) {
+					leftDrawerHelper.open();
+				}
+			} else {
+				applyCommand(new Back());
+			}
+
+		}
+
+		@Override
+		public void updateIcon() {
+			if (toolbarResolver != null) {
+
+				if (fragmentManager.getBackStackEntryCount() < 1 && activity.isTaskRoot()) {
+					toolbarResolver.showHomeIcon();
+				} else {
+					toolbarResolver.showBackIcon();
+				}
+			}
+		}
+	};
+
+	public BaseNavigator(AppCompatActivity activity, int containerId,
+	                     @Nullable ToolbarResolver toolbarResolver,
+	                     @Nullable LeftDrawerHelper leftDrawerHelper
+	) {
+		super(activity, containerId);
+		this.activity = activity;
+		this.containerId = containerId;
+		this.toolbarResolver = toolbarResolver;
+		this.leftDrawerHelper = leftDrawerHelper;
+		fragmentManager = activity.getSupportFragmentManager();
+	}
+
+	@Override
+	protected void exit() {
+		if (doubleBackToExitPressedOnce || !activity.isTaskRoot()) {
+			activity.finish();
+		} else {
+			Toast.makeText(activity, R.string.toast_exit, Toast.LENGTH_SHORT)
+					.show();
+
+			doubleBackToExitPressedOnce = true;
+			Completable.complete()
+					.delay(1000, TimeUnit.MILLISECONDS)
+					.subscribe(new Action() {
+						@Override
+						public void run() throws Exception {
+							doubleBackToExitPressedOnce = false;
+						}
+					});
+		}
+	}
+
+	@Override
+	public void applyCommand(@Nullable Command command) {
+
+		if (command instanceof Back) {
+			OnBackPressedListener listener = (OnBackPressedListener) getCurrentFragment();
+			if (listener != null && listener.onBackPressed()) {
+				return;
+
+			}
+
+			if (1 < fragmentManager.getBackStackEntryCount()) {
+				clearAndClose(command);
+				super.applyCommand(command);
+				if (toolbarResolver != null) {
+
+					int backStackEntryCount = fragmentManager.getBackStackEntryCount();
+					if (1 < backStackEntryCount) {
+						toolbarResolver.showBackIcon();
+					} else {
+						toolbarResolver.showHomeIcon();
+					}
+				}
+
+			} else {
+				exit();
+			}
+		} else {
+			clearAndClose(command);
+			super.applyCommand(command);
+			value.updateIcon();
+		}
+	}
+
+	private void clearAndClose(@Nullable Command command) {
+		if (!(command instanceof Back)) {
+			if (toolbarResolver != null) {
+				toolbarResolver.clear();
+			}
+			if (leftDrawerHelper != null) {
+				leftDrawerHelper.close();
+			}
+		}
+	}
+
+	@Nullable
+	private Fragment getCurrentFragment() {
+		return fragmentManager.findFragmentById(containerId);
+	}
+
+	public int getInitPriority() {
+		return INeedActivityViewNotify.INIT_PRIORITY_THIRD;
+	}
+
+	@Override
+	public void onInit() {
+		if (getCurrentFragment() == null) {
+			String mainScreenKey = getMainScreenKey();
+			if (mainScreenKey != null)
+				applyCommand(new Forward(mainScreenKey, null));
+		}
+		value.updateIcon();
+		if (toolbarResolver != null) {
+			toolbarResolver.setCallback(value);
+		}
+
+		if (leftDrawerHelper != null) {
+			if (fragmentManager.findFragmentById(leftDrawerHelper.getDrawerContentFrame()) == null) {
+				Fragment drawer = getDrawer();
+				if (drawer != null) {
+
+					fragmentManager.beginTransaction()
+							.add(leftDrawerHelper.getDrawerContentFrame(), drawer)
+							.commit();
+				}
+			}
+		}
+
+	}
+
+	@Nullable
+	protected abstract Fragment getDrawer();
+
+
+	protected abstract String getMainScreenKey();
+
+}
--- kotlinapp/src/main/java/com/github/mrvilkaman/ui/container/MainNavigator.kt
+++ kotlinapp/src/main/java/com/github/mrvilkaman/ui/container/MainNavigator.kt
@@ -3,33 +3,30 @@ package com.github.mrvilkaman.ui.container
 import android.content.Intent
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
-import android.widget.Toast
-import com.github.mrvilkaman.core.R
-import com.github.mrvilkaman.di.INeedActivityViewNotify
-import com.github.mrvilkaman.presentationlayer.fragments.core.OnBackPressedListener
+import com.github.mrvilkaman.presentationlayer.resolution.BaseNavigator
 import com.github.mrvilkaman.presentationlayer.resolution.drawer.LeftDrawerHelper
 import com.github.mrvilkaman.presentationlayer.resolution.toolbar.ToolbarResolver
 import com.github.mrvilkaman.ui.screens.ScreenKey
 import com.github.mrvilkaman.ui.screens.drawer.DrawerScreenFragment
 import com.github.mrvilkaman.ui.screens.testfrags.*
-import io.reactivex.Completable
-import ru.terrakok.cicerone.android.SupportAppNavigator
-import ru.terrakok.cicerone.commands.Back
-import ru.terrakok.cicerone.commands.Command
-import ru.terrakok.cicerone.commands.Forward
-import ru.terrakok.cicerone.commands.SystemMessage
-import java.util.concurrent.TimeUnit
 
 
 class MainNavigator(
-        private val activity: AppCompatActivity,
-        private val containerId: Int,
-        private val toolbarResolver: ToolbarResolver?,
-        private val leftDrawerHelper: LeftDrawerHelper?
+        activity: AppCompatActivity,
+        containerId: Int,
+        toolbarResolver: ToolbarResolver?,
+        leftDrawerHelper: LeftDrawerHelper?
 
-) : SupportAppNavigator(activity, containerId), INeedActivityViewNotify {
+) : BaseNavigator(
+        activity,
+        containerId,
+        toolbarResolver,
+        leftDrawerHelper
+) {
+    override fun getDrawer(): Fragment? =
+        DrawerScreenFragment.open()
 
-    private val fragmentManager = activity.supportFragmentManager
+
 
     override fun createActivityIntent(screenKey: String, data: Any?): Intent? {
         return null
@@ -45,103 +42,6 @@ class MainNavigator(
             null
     }
 
-    protected fun getMainScreenKey(): String? = ScreenKey.FRAG1
-
-
-    //core part
-
-
-    private var doubleBackToExitPressedOnce: Boolean = false
-    override fun exit() {
-        if (doubleBackToExitPressedOnce || !activity.isTaskRoot) {
-            activity.finish()
-        } else {
-            Toast.makeText(activity, R.string.toast_exit, Toast.LENGTH_SHORT).show()
-
-            doubleBackToExitPressedOnce = true
-            Completable.complete().delay(1000, TimeUnit.MILLISECONDS)
-                    .subscribe { doubleBackToExitPressedOnce = false }
-        }
-    }
-
-    override fun applyCommand(command: Command?) {
-
-        if (command is Back) {
-            val listener = getCurrentFragment() as? OnBackPressedListener
-            val handleFragment = listener?.onBackPressed() ?: false
-            if (handleFragment) {
-                return
-            }
-
-
-            if (1 < fragmentManager.backStackEntryCount) {
-                clearAndClose(command)
-                super.applyCommand(command)
-                val backStackEntryCount = fragmentManager.backStackEntryCount
-                if (1 < backStackEntryCount) {
-                    toolbarResolver?.showBackIcon()
-                } else {
-                    toolbarResolver?.showHomeIcon()
-                }
-
-            } else {
-                exit()
-            }
-        } else {
-            clearAndClose(command)
-            super.applyCommand(command)
-            value.updateIcon()
-        }
-    }
-
-    private fun clearAndClose(command: Command?) {
-        if (command !is SystemMessage) {
-            toolbarResolver?.clear()
-            leftDrawerHelper?.close()
-        }
-    }
-
-    private fun getCurrentFragment(): Fragment? = fragmentManager.findFragmentById(containerId)
-
-    override fun getInitPriority(): Int {
-        return INeedActivityViewNotify.INIT_PRIORITY_THIRD
-    }
-
-    override fun onInit() {
-        if (getCurrentFragment() == null) {
-            val mainScreenKey = getMainScreenKey()
-            if (mainScreenKey != null)
-                applyCommand(Forward(mainScreenKey, null))
-        }
-        value.updateIcon()
-        toolbarResolver?.setCallback(value)
-
-        if (leftDrawerHelper != null) {
-            if (fragmentManager.findFragmentById(leftDrawerHelper.drawerContentFrame) == null) {
-                fragmentManager.beginTransaction().add(leftDrawerHelper.drawerContentFrame, DrawerScreenFragment.open() as Fragment).commit()
-            }
-        }
-
-    }
-
-    private val value: ToolbarResolver.ToolbarResolverCallback = object : ToolbarResolver.ToolbarResolverCallback {
-        override fun onClickHome() {
-            if (fragmentManager.backStackEntryCount <= 1 && activity.isTaskRoot) {
-                leftDrawerHelper?.open()
-            } else {
-                applyCommand(Back())
-//                    activityView.hideKeyboard()
-            }
-        }
-
-        override fun updateIcon() {
-            if (fragmentManager.backStackEntryCount < 1 && activity.isTaskRoot) {
-                toolbarResolver?.showHomeIcon()
-            } else {
-                toolbarResolver?.showBackIcon()
-            }
-        }
-
-    }
+    override fun getMainScreenKey(): String? = ScreenKey.FRAG1
 
 }
